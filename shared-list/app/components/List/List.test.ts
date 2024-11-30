99c7907eb855040a3b368a3578dfb712329cd0b4
Chen Mor Yosef <chenm@island.io>
2024-11-30 17:31:56 +0200

    #3: Implement List + add tests

---
+++ shared-list/app/components/List/List.test.ts
@@ -0,0 +1,46 @@
+import { IList } from './IList';
+import { List } from './List';
+import Item from '../Item/Item';
+
+describe('Tests for List class', () => {
+  let list: IList;
+  const listName = 'test list';
+  const itemName = 'test item';
+  const itemDescription = 'test description';
+
+  beforeEach(() => {
+    list = new List(listName);
+  });
+
+  test('should create a list with correct properties', () => {
+    expect(list.id).toBeGreaterThan(0);
+    expect(list.name).toBe(listName);
+    expect(list.items).toEqual([]);
+  });
+
+  test('should add an item to the list', () => {
+    const item = new Item(itemName, itemDescription);
+    list.add(item);
+    expect(list.items.length).toBe(1);
+    expect(list.items[0]).toBe(item);
+  });
+
+  test('should remove an item from the list', () => {
+    const item = new Item(itemName, itemDescription);
+    const item2 = new Item(itemName, itemDescription);
+    list.add(item);
+    list.add(item2);
+    expect(list.items.length).toBe(2);
+
+    list.remove(item);
+    expect(list.items.length).toBe(1);
+  });
+
+  test('should modify an item in the list', () => {
+    const item = new Item(itemName, itemDescription);
+    list.add(item);
+    list.modify(item, 'new name', 'new description');
+    expect(list.items.length).toBe(1);
+    expect(list.items[0].name).toBe('new name');
+  });
+});
---
+++ shared-list/app/components/List/List.ts
@@ -0,0 +1,37 @@
+import { IItem } from '../Item/IItem';
+import { IList } from './IList';
+
+export class List implements IList {
+  id: number;
+  name?: string;
+  items: IItem[];
+
+  constructor(name?: string) {
+    this.id = this.generateId();
+    this.name = name;
+    this.items = [];
+  }
+
+  private generateId(): number {
+    return Math.floor(Math.random() * 1000000);
+  }
+
+  add(item: IItem): void {
+    this.items.push(item);
+  }
+
+  remove(item: IItem): void {
+    const index = this.items.indexOf(item);
+    if (index > -1) {
+      this.items.splice(index, 1);
+    }
+  }
+
+  modify(item: IItem, name?: string, description?: string): void {
+    const index = this.items.findIndex((i) => i.id === item.id);
+    if (index > -1) {
+      if (name) this.items[index].setName(name);
+      if (description) this.items[index].setDescription(description);
+    }
+  }
+}
