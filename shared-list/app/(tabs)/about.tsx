8e3cba1d52628ad8ff81318e1b8bea7ed8fac6ee
Chen Mor Yosef <chenm@island.io>
2024-12-19 23:28:53 +0200

    #12: Update about page

--- shared-list/app/(tabs)/about.tsx
+++ shared-list/app/(tabs)/about.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from 'react-native';
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Chen needs to write about info...</Text>
+      <Text style={styles.text}>Shared List helps you be more organized, contorl all your lists and even be able to share them with others.</Text>
     </View>
   );
 }
